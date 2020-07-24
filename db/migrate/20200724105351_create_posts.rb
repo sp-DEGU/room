d4839abc0623c2074a26e076a0c48826ba4db6f4
sp_DEGU <degudegu2510@gmail.com>
2020-07-24 20:44:37 +0900

    create db

---
+++ db/migrate/20200724105351_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :title
+      t.text :detail
+      t.datetime :time_start
+      t.datetime :time_end
+      t.text :user
+      t.text :url
+
+      t.timestamps
+    end
+  end
+end
