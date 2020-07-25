43d9da294413dcda699aed8f0ac95a398bbeacfd
sp_DEGU <degudegu2510@gmail.com>
2020-07-25 13:32:04 +0900

    create

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,27 @@
 class HomeController < ApplicationController
   def index
-    
+    @nowposts = Post.all.order(created_at: :desc)
+    @lateposts = Post.all.order(created_at: :desc)
+  end
+
+  def edit
+  
+  end
+
+  def create
+    @post = Post.new(
+      title: params[:title],
+      detail: params[:detail],
+      time_start: params[:timeStart],
+      time_end: params[:timeEnd],
+      user: params[:user],
+      url: params[:url]
+    )
+    if @post.save
+      flash[:notice] = "予定を追加しました！"
+      redirect_to("/home/index")
+    else
+      render("/home/edit")
+    end
   end
 end
